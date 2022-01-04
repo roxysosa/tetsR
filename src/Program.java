06a55f35f51dd1c3953c855986645ac8e52f74cb
roxysosa <roxy12.sosa@gmail.com>
2022-01-04 11:56:20 +0100

    comit 2

--- src/Program.java
+++ src/Program.java
@@ -3,6 +3,18 @@ import com.gurock.testrail.APIException;
 import java.util.Map;
 import java.util.HashMap;
 import org.json.simple.JSONObject;
+public class Program
+{
+    public static void main(String[] args) throws Exception
+    {
+        APIClient client = new APIClient("https://ceztherm.testrail.io");
+        client.setUser("rosamis.fortun@idneo.com");
+        client.setPassword("xmr");
+        JSONObject c = (JSONObject) client.sendGet("get_case/30909");
+        System.out.println(c.get("title"));
 
-public class Program {
+
+    }
 }
+
+
